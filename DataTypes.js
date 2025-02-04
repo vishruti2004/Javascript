6a73a10cf5eb42b388ad16fb55ddc1dce94befb0
vishruti2004 <vishrutimalaviya282@gmail.com>
2025-02-04 18:17:02 +0530

    javascript

---
+++ DataTypes.js
@@ -0,0 +1,15 @@
+const marks = 100;
+const name = "vishu";
+const islogin = true;
+let city;
+const bignumber = "123456789";
+const abc =  Symbol("282");
+const prq  = Symbol("282");
+
+
+console.log(typeof marks);
+console.log(typeof name);
+console.log(typeof city);
+console.log(typeof bignumber);
+console.log(typeof abc);
+console.log( abc == prq);
