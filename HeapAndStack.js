30edaa528ea28e408fcca2fe169dc366c366b23a
vishruti2004 <vishrutimalaviya282@gmail.com>
2025-02-04 18:44:40 +0530

    javascript

---
+++ HeapAndStack.js
@@ -0,0 +1,20 @@
+let user1 = "vishruti";
+
+let user2 = user1;
+user2 = "malaviya";
+console.log(user1);
+console.log(user2);
+
+
+const userr1 = 
+{
+        email:"vishumalaviya282@gmail.com",
+        phone_num:1234567890
+}
+
+let userr2 = userr1;
+
+userr2.email = "vishumalaviya";
+
+console.log(userr1.email);
+console.log(userr2.email);
