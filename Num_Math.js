b32d76ec75eb8c56c887d197e10269b3bc619364
vishruti2004 <vishrutimalaviya282@gmail.com>
2025-02-05 19:56:54 +0530

    javascript

---
+++ Num_Math.js
@@ -0,0 +1,24 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+//maths
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
---
+++ String.js
@@ -0,0 +1,11 @@
+//let String = "vishu";
+
+
+let myname = new String("vishu");
+
+console.log(myname[2]);
+
+console.log(myname.toUpperCase());
+console.log(myname.charAt(2));
+console.log(myname.indexOf('v'));
+console.log(myname.substring(1,3));
