04b2a227a24b7c2c03ad582e890a46e1f798b3c0
vishruti2004 <vishrutimalaviya282@gmail.com>
2025-02-10 18:16:20 +0530

    javascript

---
+++ scope.js
@@ -0,0 +1,55 @@
+if (true) {
+    let a = 20;
+    const b = 30;
+    var c = 10;
+}
+
+//console.log(a);
+//console.log(b);
+console.log(c);
+
+
+let x = 10;
+
+if (true) {
+    let x = 20;
+    console.log(x);
+
+}
+
+console.log(x);
+
+function one() {
+    const name = "vishu";
+
+    function two() {
+        const surname = "malaviya";
+        console.log(name);
+
+    }
+    //console.log(surname);
+
+    two();
+
+}
+
+one()
+
+
+if(true)
+{
+    const username = "vishu";
+    if(username == "vishu")
+    {
+        const password=123;
+        console.log(username+password);
+        
+    }
+   // console.log(password);
+    console.log(username);
+
+}
+//console.log(username);
+
+
+
